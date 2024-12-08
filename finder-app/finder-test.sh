83004c581c0f86769701115d179e814370b29d77
William Diment <widi9545@colorado.edu>
2024-12-08 16:31:01 -0700

    Update finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -59,7 +59,7 @@ do
 done
 
 OUTPUTSTRING=$(/etc/finder-app/finder.sh "$WRITEDIR" "$WRITESTR")
-
+echo $OUTPUTSTRING > /tmp/assignment4-result.txt
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
