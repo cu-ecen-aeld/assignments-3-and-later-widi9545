f339d6f9e81df795f21a028463b803c5535b8926
William Diment <widi9545@colorado.edu>
2025-07-27 19:49:16 -0600

    testing mpi aesd-char-device'

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -17,7 +17,6 @@
 #else
     #define FILE_LOCATION ("/tmp/aesdsocket")
 #endif
-int mpid, mpnum;
 #define SIGINT 2
 
 
@@ -247,6 +246,7 @@ int main(int argc, char * argv[]){
     int child_process = 0;
     int handler = 0;
     int process_Rank, size_Of_Cluster;
+    int mpid, mpnum;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&child_process);
     MPI_Comm_size(MPI_COMM_WORLD,&mpnum);
